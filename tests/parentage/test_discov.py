1a53da4f7f1468df6f43c7ce34f21743c081eafd
IgorekLoschinin <igor.loschinin@gmail.com>
2026-07-12 22:31:34 +0300

    Refactoring and update test for parentage

--- tests/parentage/test_discov.py
+++ tests/parentage/test_discov.py
@@ -3,7 +3,10 @@
 __author__ = "Igor Loschinin (igor.loschinin@gmail.com)"
 
 from . import DIR_DATA
-from snptools.src.snplib.parentage import Discovery, isag_disc
+from snptools.src.snplib.parentage import (
+	Discovery,
+	isag_disc
+)
 
 import pytest
 import pandas as pd
@@ -31,9 +34,9 @@ class TestDiscovery(object):
 			parents="EE10512586",
 			snp_name_col="SNP_Name"
 		) is None
-		assert obj_discovery.num_conflicts == 77
-		assert obj_discovery.status == "Excluded"
-		assert obj_discovery.perc_conflicts == 14.86
+		assert obj_discovery.status_sing == "Excluded"
+		assert obj_discovery.num_conflicts_sing == 77
+		assert obj_discovery.perc_conflicts_sing == 14.89
 
 	def test_search_parent_1(self, data: pd.DataFrame) -> None:
 		"""
@@ -50,9 +53,9 @@ class TestDiscovery(object):
 				parents="EE10512586",
 				snp_name_col="SNP_Name"
 			)
-		assert obj_discovery.status is None
-		assert obj_discovery.num_conflicts is None
-		assert obj_discovery.perc_conflicts is None
+		assert obj_discovery.status_sing is None
+		assert obj_discovery.num_conflicts_sing is None
+		assert obj_discovery.perc_conflicts_sing is None
 
 	def test_search_parent_2(
 		self, data: pd.DataFrame, obj_discovery: Discovery
@@ -62,18 +65,15 @@ class TestDiscovery(object):
 		less than the established value.
 		"""
 
-		with pytest.raises(
-			Exception, match="Calf call rate is low."
-		):
-			obj_discovery.search_parent(
-				data=data[:-100],
-				descendant="BY000041988163",
-				parents="EE10512586",
-				snp_name_col="SNP_Name"
-			)
-		assert obj_discovery.status is None
-		assert obj_discovery.num_conflicts is None
-		assert obj_discovery.perc_conflicts is None
+		assert obj_discovery.search_parent(
+			data=data[:-150],
+			descendant="BY000041988163",
+			parents="EE10512586",
+			snp_name_col="SNP_Name"
+		) is None
+		assert obj_discovery.status_sing == 'Not Discovered'
+		assert obj_discovery.num_conflicts_sing == 58
+		assert obj_discovery.perc_conflicts_sing == 15.72
 
 	def test_search_parent_3(
 		self, data: pd.DataFrame, obj_discovery: Discovery
@@ -90,9 +90,9 @@ class TestDiscovery(object):
 				parents="EE10512586",
 				snp_name_col="SNP_Name"
 			)
-		assert obj_discovery.status is None
-		assert obj_discovery.num_conflicts is None
-		assert obj_discovery.perc_conflicts is None
+		assert obj_discovery.status_sing is None
+		assert obj_discovery.num_conflicts_sing is None
+		assert obj_discovery.perc_conflicts_sing is None
 
 		# For parents
 		with pytest.raises(KeyError):
@@ -102,9 +102,9 @@ class TestDiscovery(object):
 				parents="EE105125864",
 				snp_name_col="SNP_Name"
 			)
-		assert obj_discovery.status is None
-		assert obj_discovery.num_conflicts is None
-		assert obj_discovery.perc_conflicts is None
+		assert obj_discovery.status_sing is None
+		assert obj_discovery.num_conflicts_sing is None
+		assert obj_discovery.perc_conflicts_sing is None
 
 	def test_search_parent_4(
 		self, data: pd.DataFrame, obj_discovery: Discovery
@@ -114,18 +114,15 @@ class TestDiscovery(object):
 		"""
 		data[["BY000041988163", "EE10512586"]] = 5
 
-		with pytest.raises(
-			Exception, match="Calf call rate is low."
-		):
-			obj_discovery.search_parent(
-				data=data,
-				descendant="BY000041988163",
-				parents="EE10512586",
-				snp_name_col="SNP_Name"
-			)
-		assert obj_discovery.status is None
-		assert obj_discovery.num_conflicts is None
-		assert obj_discovery.perc_conflicts is None
+		assert obj_discovery.search_parent(
+			data=data,
+			descendant="BY000041988163",
+			parents="EE10512586",
+			snp_name_col="SNP_Name"
+		) is None
+		assert obj_discovery.status_sing == 'Not Discovered'
+		assert obj_discovery.num_conflicts_sing == 0
+		assert obj_discovery.perc_conflicts_sing is None
 
 	def test_search_parent_5(
 			self, data: pd.DataFrame, obj_discovery: Discovery
@@ -141,9 +138,9 @@ class TestDiscovery(object):
 			parents="EE10512586",
 			snp_name_col="SNP_Name"
 		)
-		assert obj_discovery.status == "Discovered"
-		assert obj_discovery.num_conflicts == 0
-		assert obj_discovery.perc_conflicts == 0.0
+		assert obj_discovery.status_sing == "Discovered"
+		assert obj_discovery.num_conflicts_sing == 0
+		assert obj_discovery.perc_conflicts_sing == 0.0
 
 	def test_search_parent_6(
 			self, data: pd.DataFrame, obj_discovery: Discovery
@@ -151,7 +148,7 @@ class TestDiscovery(object):
 		"""
 		Partial match test.
 		"""
-		data.loc[202:, "EE10512586"] = 1
+		data.loc[150:, "EE10512586"] = 1
 
 		obj_discovery.search_parent(
 			data=data,
@@ -159,6 +156,146 @@ class TestDiscovery(object):
 			parents="EE10512586",
 			snp_name_col="SNP_Name"
 		)
-		assert obj_discovery.status == "Doubtful"
-		assert obj_discovery.num_conflicts == 14
-		assert obj_discovery.perc_conflicts == 2.70
+		assert obj_discovery.status_sing == "Possible"
+		assert obj_discovery.num_conflicts_sing == 5
+		assert obj_discovery.perc_conflicts_sing == 0.97
+
+	# def test_search_mating_confirmed(
+	# 		self, data: pd.DataFrame, obj_discovery: Discovery
+	# ) -> None:
+	# 	"""
+	# 	Test mating with 0 conflicts (Confirmed).
+	# 	"""
+	# 	data_copy = data.copy()
+	# 	snp_name_col = "SNP_Name"
+	# 	descendant = "BY000041988163"
+	# 	sire = "EE10512586"
+	#
+	# 	data_copy = data_copy.set_index(snp_name_col)
+	# 	desc_vals = data_copy[descendant]
+	# 	sire_vals = data_copy[sire]
+	#
+	# 	# Create a dam with 0 conflicts
+	# 	dam_vals = sire_vals.copy()
+	# 	mask = (desc_vals == 1) & (sire_vals.isin([0, 2]))
+	# 	dam_vals[mask] = 2 - sire_vals[mask]
+	#
+	# 	data_copy["Dam_0"] = dam_vals
+	# 	data_copy = data_copy.reset_index()
+	#
+	# 	obj_discovery.search_mating(
+	# 		data=data_copy,
+	# 		descendant=descendant,
+	# 		sire=sire,
+	# 		dam="Dam_0",
+	# 		snp_name_col=snp_name_col
+	# 	)
+	#
+	# 	markers = obj_discovery._Discovery__isag_markers
+	# 	num_common, num_conflicts, perc_conflicts = _calculate_step2(
+	# 		data_copy, descendant, sire, "Dam_0", snp_name_col, markers
+	# 	)
+	#
+	# 	assert obj_discovery.status_step2 == "Confirmed"
+	# 	assert obj_discovery.num_conflicts_step2 == num_conflicts
+	# 	assert obj_discovery.perc_conflicts_step2 == perc_conflicts
+	#
+	# def test_search_mating_possible(
+	# 		self, data: pd.DataFrame, obj_discovery: Discovery
+	# ) -> None:
+	# 	"""
+	# 	Test mating with ~1.63% conflicts (Possible).
+	# 	"""
+	# 	data_copy = data.copy()
+	# 	snp_name_col = "SNP_Name"
+	# 	descendant = "BY000041988163"
+	# 	sire = "EE10512586"
+	#
+	# 	data_copy = data_copy.set_index(snp_name_col)
+	# 	desc_vals = data_copy[descendant]
+	# 	sire_vals = data_copy[sire]
+	#
+	# 	# Start with 0 conflicts dam
+	# 	dam_vals = sire_vals.copy()
+	# 	mask = (desc_vals == 1) & (sire_vals.isin([0, 2]))
+	# 	dam_vals[mask] = 2 - sire_vals[mask]
+	#
+	# 	# Force 10 conflicts
+	# 	conflict_snp_names = data_copy.index[
+	# 		(desc_vals == 1) & (sire_vals.isin([0, 2])) & (desc_vals != 5) & (
+	# 					sire_vals != 5)]
+	# 	dam_vals.loc[conflict_snp_names[:10]] = sire_vals.loc[
+	# 		conflict_snp_names[:10]]
+	#
+	# 	data_copy["Dam_Possible"] = dam_vals
+	# 	data_copy = data_copy.reset_index()
+	#
+	# 	obj_discovery.search_mating(
+	# 		data=data_copy,
+	# 		descendant=descendant,
+	# 		sire=sire,
+	# 		dam="Dam_Possible",
+	# 		snp_name_col=snp_name_col
+	# 	)
+	#
+	# 	markers = obj_discovery._Discovery__isag_markers
+	# 	num_common, num_conflicts, perc_conflicts = _calculate_step2(
+	# 		data_copy, descendant, sire, "Dam_Possible", snp_name_col, markers
+	# 	)
+	#
+	# 	assert obj_discovery.status_step2 == "Possible"
+	# 	assert obj_discovery.num_conflicts_step2 == num_conflicts
+	# 	assert obj_discovery.perc_conflicts_step2 == perc_conflicts
+	#
+	# def test_search_mating_excluded(
+	# 		self, data: pd.DataFrame, obj_discovery: Discovery
+	# ) -> None:
+	# 	"""
+	# 	Test mating with high conflicts (Excluded).
+	# 	"""
+	# 	data_copy = data.copy()
+	# 	snp_name_col = "SNP_Name"
+	# 	descendant = "BY000041988163"
+	# 	sire = "EE10512586"
+	#
+	# 	# Dam is identical to sire, which will cause many conflicts
+	# 	data_copy["Dam_Excluded"] = data_copy[sire]
+	#
+	# 	obj_discovery.search_mating(
+	# 		data=data_copy,
+	# 		descendant=descendant,
+	# 		sire=sire,
+	# 		dam="Dam_Excluded",
+	# 		snp_name_col=snp_name_col
+	# 	)
+	#
+	# 	markers = obj_discovery._Discovery__isag_markers
+	# 	num_common, num_conflicts, perc_conflicts = _calculate_step2(
+	# 		data_copy, descendant, sire, "Dam_Excluded", snp_name_col, markers
+	# 	)
+	#
+	# 	assert obj_discovery.status_step2 == "Excluded"
+	# 	assert obj_discovery.num_conflicts_step2 == num_conflicts
+	# 	assert obj_discovery.perc_conflicts_step2 == perc_conflicts
+	#
+	# def test_search_mating_not_checked(
+	# 		self, data: pd.DataFrame, obj_discovery: Discovery
+	# ) -> None:
+	# 	"""
+	# 	Status 'Not Checked' when the number of common markers for trio is less than 400.
+	# 	"""
+	# 	data_copy = data.copy()
+	# 	data_copy["Dam_Fake"] = data_copy["EE10512586"]
+	#
+	# 	obj_discovery.search_mating(
+	# 		data=data_copy[:-250],
+	# 		# Slicing to ensure < 400 common SNPs for trio
+	# 		descendant="BY000041988163",
+	# 		sire="EE10512586",
+	# 		dam="Dam_Fake",
+	# 		snp_name_col="SNP_Name"
+	# 	)
+	#
+	# 	assert obj_discovery.status_step2 == "Not Checked"
+	# 	assert obj_discovery.num_conflicts_step2 == 0
+	# 	assert obj_discovery.perc_conflicts_step2 is None
--- tests/parentage/test_verif.py
+++ tests/parentage/test_verif.py
@@ -3,7 +3,10 @@
 __author__ = "Igor Loschinin (igor.loschinin@gmail.com)"
 
 from . import DIR_DATA
-from snptools.src.snplib.parentage import Verification, isag_verif
+from snptools.src.snplib.parentage import (
+	Verification,
+	isag_verif
+)
 
 import pytest
 import pandas as pd
@@ -31,8 +34,8 @@ class TestVerification(object):
 			parent="EE10512586",
 			snp_name_col="SNP_Name"
 		) is None
-		assert obj_verification.num_conflicts == 31
-		assert obj_verification.status == "Excluded"
+		assert obj_verification.num_conflicts_sing == 31
+		assert obj_verification.status_sing == "Excluded"
 
 	def test_check_on_1(self, data: pd.DataFrame) -> None:
 		"""
@@ -49,8 +52,8 @@ class TestVerification(object):
 				parent="EE10512586",
 				snp_name_col="SNP_Name"
 			)
-		assert obj_verification.status is None
-		assert obj_verification.num_conflicts is None
+		assert obj_verification.status_sing is None
+		assert obj_verification.num_conflicts_sing is None
 
 	def test_check_on_2(
 		self, data: pd.DataFrame, obj_verification: Verification
@@ -59,17 +62,14 @@ class TestVerification(object):
 		Exception for low call rate in both animals.
 		"""
 
-		with pytest.raises(
-			Exception, match="Calf and parent have low call rate"
-		):
-			obj_verification.check_on(
-				data=data[:-100],
-				descendant="BY000041988163",
-				parent="EE10512586",
-				snp_name_col="SNP_Name"
-			)
-		assert obj_verification.status is None
-		assert obj_verification.num_conflicts is None
+		assert obj_verification.check_on(
+			data=data[:-100],
+			descendant="BY000041988163",
+			parent="EE10512586",
+			snp_name_col="SNP_Name"
+		) is None
+		assert obj_verification.status_sing == 'Not Verified'
+		assert obj_verification.num_conflicts_sing is None
 
 	def test_check_on_3(
 		self, data: pd.DataFrame, obj_verification: Verification
@@ -81,17 +81,14 @@ class TestVerification(object):
 		data.loc[228:, 'BY000041988163'] = 5
 		data.loc[239:, 'EE10512586'] = 5
 
-		with pytest.raises(
-			Exception, match="Pair call rate is low"
-		):
-			obj_verification.check_on(
-				data=data,
-				descendant="BY000041988163",
-				parent="EE10512586",
-				snp_name_col="SNP_Name"
-			)
-		assert obj_verification.status is None
-		assert obj_verification.num_conflicts is None
+		assert obj_verification.check_on(
+			data=data,
+			descendant="BY000041988163",
+			parent="EE10512586",
+			snp_name_col="SNP_Name"
+		) is None
+		assert obj_verification.status_sing == 'Not Checked'
+		assert obj_verification.num_conflicts_sing is None
 
 	def test_search_parent_4(
 		self, data: pd.DataFrame, obj_verification: Verification
@@ -108,8 +105,8 @@ class TestVerification(object):
 				parent="EE10512586",
 				snp_name_col="SNP_Name"
 			)
-		assert obj_verification.status is None
-		assert obj_verification.num_conflicts is None
+		assert obj_verification.status_sing is None
+		assert obj_verification.num_conflicts_sing is None
 
 		# For parents
 		with pytest.raises(KeyError):
@@ -119,8 +116,8 @@ class TestVerification(object):
 				parent="EE105125864",
 				snp_name_col="SNP_Name"
 			)
-		assert obj_verification.status is None
-		assert obj_verification.num_conflicts is None
+		assert obj_verification.status_sing is None
+		assert obj_verification.num_conflicts_sing is None
 
 	def test_search_parent_5(
 		self, data: pd.DataFrame, obj_verification: Verification
@@ -130,17 +127,14 @@ class TestVerification(object):
 		"""
 		data[["BY000041988163", "EE10512586"]] = 5
 
-		with pytest.raises(
-			Exception, match="Calf and parent have low call rate"
-		):
-			obj_verification.check_on(
-				data=data,
-				descendant="BY000041988163",
-				parent="EE10512586",
-				snp_name_col="SNP_Name"
-			)
-		assert obj_verification.status is None
-		assert obj_verification.num_conflicts is None
+		obj_verification.check_on(
+			data=data,
+			descendant="BY000041988163",
+			parent="EE10512586",
+			snp_name_col="SNP_Name"
+		)
+		assert obj_verification.status_sing == 'Not Verified'
+		assert obj_verification.num_conflicts_sing is None
 
 	def test_search_parent_6(
 			self, data: pd.DataFrame, obj_verification: Verification
@@ -156,5 +150,121 @@ class TestVerification(object):
 			parent="EE10512586",
 			snp_name_col="SNP_Name"
 		)
-		assert obj_verification.status == "Accept"
-		assert obj_verification.num_conflicts == 0
+		assert obj_verification.status_sing == "Accept"
+		assert obj_verification.num_conflicts_sing == 0
+
+	def test_search_parent_7(
+			self, data: pd.DataFrame, obj_verification: Verification
+	) -> None:
+		"""
+		Test when there is a Doubtful
+		"""
+		data.loc[70:, "EE10512586"] = 1
+
+		obj_verification.check_on(
+			data=data,
+			descendant="BY000041988163",
+			parent="EE10512586",
+			snp_name_col="SNP_Name"
+		)
+		assert obj_verification.status_sing == "Doubtful"
+		assert obj_verification.num_conflicts_sing == 5
+
+	# # ==================== STEP 2 TESTS ====================
+	#
+	# def test_check_mating_accepted(self, data: pd.DataFrame,
+	# 							   obj_verification: Verification) -> None:
+	# 	"""Test 0-3 conflicts -> Mating Accepted."""
+	# 	data_copy = data.copy()
+	# 	snp_col = "SNP_Name"
+	# 	desc = "BY000041988163"
+	# 	sire = "EE10512586"
+	#
+	# 	# Создаем мать с 0 конфликтами
+	# 	sample = data_copy.set_index(snp_col)
+	# 	desc_vals = sample[desc]
+	# 	sire_vals = sample[sire]
+	# 	dam_vals = sire_vals.copy()
+	#
+	# 	# Исправляем потенциальные конфликты (оба родителя гомозиготны, потомок гетерозиготен)
+	# 	mask = (desc_vals == 1) & (sire_vals.isin([0, 2])) & (
+	# 				desc_vals != 5) & (sire_vals != 5)
+	# 	dam_vals[mask] = 2 - sire_vals[mask]
+	#
+	# 	data_copy["Dam_OK"] = dam_vals.values
+	# 	data_copy = data_copy.reset_index()
+	#
+	# 	obj_verification.check_mating(data_copy, desc, sire, "Dam_OK", snp_col)
+	# 	assert obj_verification.status_step2 == 'Mating Accepted'
+	# 	assert obj_verification.num_conflicts_step2 <= 3
+	#
+	# def test_check_mating_doubtful(self, data: pd.DataFrame,
+	# 							   obj_verification: Verification) -> None:
+	# 	"""Test 4-7 conflicts -> Mating Doubtful."""
+	# 	data_copy = data.copy()
+	# 	snp_col = "SNP_Name"
+	# 	desc = "BY000041988163"
+	# 	sire = "EE10512586"
+	#
+	# 	sample = data_copy.set_index(snp_col)
+	# 	desc_vals = sample[desc]
+	# 	sire_vals = sample[sire]
+	# 	dam_vals = sire_vals.copy()
+	#
+	# 	# Сначала убираем все конфликты
+	# 	mask = (desc_vals == 1) & (sire_vals.isin([0, 2])) & (
+	# 				desc_vals != 5) & (sire_vals != 5)
+	# 	dam_vals[mask] = 2 - sire_vals[mask]
+	#
+	# 	# Теперь искусственно создаем ровно 5 конфликтов
+	# 	conflict_indices = data_copy.index[
+	# 						   (desc_vals == 1) & (sire_vals.isin([0, 2])) & (
+	# 									   desc_vals != 5) & (sire_vals != 5)][
+	# 					   :5]
+	# 	dam_vals.iloc[conflict_indices] = sire_vals.iloc[
+	# 		conflict_indices]  # Делаем родителей одинаковыми гомозиготами
+	#
+	# 	data_copy["Dam_Doubt"] = dam_vals.values
+	# 	data_copy = data_copy.reset_index()
+	#
+	# 	obj_verification.check_mating(data_copy, desc, sire, "Dam_Doubt",
+	# 								  snp_col)
+	# 	assert obj_verification.status_step2 == 'Mating Doubtful'
+	# 	assert obj_verification.num_conflicts_step2 == 5
+	#
+	# def test_check_mating_excluded(self, data: pd.DataFrame,
+	# 							   obj_verification: Verification) -> None:
+	# 	"""Test >7 conflicts -> Mating Excluded."""
+	# 	data_copy = data.copy()
+	# 	snp_col = "SNP_Name"
+	# 	desc = "BY000041988163"
+	# 	sire = "EE10512586"
+	#
+	# 	# Мать идентична отцу, что гарантированно создаст много конфликтов там, где потомок гетерозиготен
+	# 	data_copy["Dam_Excl"] = data_copy[sire]
+	#
+	# 	obj_verification.check_mating(data_copy, desc, sire, "Dam_Excl",
+	# 								  snp_col)
+	#
+	# 	markers = obj_verification._Verification__isag_marks
+	# 	num_conflicts, _, status = _calculate_step2(data_copy, desc, sire,
+	# 												"Dam_Excl", snp_col,
+	# 												markers)
+	#
+	# 	assert obj_verification.status_step2 == status
+	# 	assert obj_verification.num_conflicts_step2 == num_conflicts
+	# 	assert obj_verification.status_step2 == 'Mating Excluded'
+	#
+	# def test_check_mating_not_checked(self, data: pd.DataFrame,
+	# 								  obj_verification: Verification) -> None:
+	# 	"""Status 'Not Checked' when trio common SNPs < 165."""
+	# 	data_copy = data.copy()
+	# 	data_copy["Dam_Fake"] = data_copy["EE10512586"]
+	#
+	# 	# Обрезаем данные, чтобы общих SNP для трио точно не хватило
+	# 	obj_verification.check_mating(data_copy[:-150], "BY000041988163",
+	# 								  "EE10512586", "Dam_Fake", "SNP_Name")
+	#
+	# 	assert obj_verification.status_step2 == 'Not Checked'
+	# 	assert obj_verification.num_conflicts_step2 is None
+	#
