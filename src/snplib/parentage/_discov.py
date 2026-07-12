e570db3d72d1e2e440a5e6ef2db427179b527de8
IgorekLoschinin <igor.loschinin@gmail.com>
2026-07-12 22:30:46 +0300

    Update algorithm disc and verif under ICAR Feb2025

--- src/snplib/parentage/_discov.py
+++ src/snplib/parentage/_discov.py
@@ -6,97 +6,162 @@ import pandas as pd
 
 """
 Search for paternity according to ICAR recommendations
-https://www.icar.org/Documents/GenoEx/ICAR%20Guidelines%20for%20Parentage%20Verification%20and%20Parentage%20Discovery%20based%20on%20SNP.pdf
+https://www.icar.org/wp-content/uploads/documents/Guidelines-for-Parentage-Verification-and-Parentage-Discovery-Based-on-SNP-Genotypes-Feb.-2025.pdf
 """
 
 
 class Discovery(object):
-    """ Search for paternity according to ICAR recommendations
+	""" Search for paternity according to ICAR recommendations (Revised Feb 2025)
 
-    :argument isag_markers: Fixed sample of markers to confirm paternity.
-    """
+	:argument isag_markers: Fixed sample of markers to confirm paternity.
+	"""
 
-    def __init__(
-            self, isag_markers: pd.Series | list | set | None = None
-    ) -> None:
-        self.__isag_markers = isag_markers
+	def __init__(
+			self, isag_markers: pd.Series | list | set | None = None
+	) -> None:
+		self._isag_markers = isag_markers
 
-        self.__num_conflicts = None  # Number of conflicts
-        self.__perc_conflicts = None
+		# Attributes for Step 1 (Find one parent)
+		self.__num_common_sing = None
+		self.__num_conflicts_sing = None
+		self.__perc_conflicts_sing = None
+		self.__status_sing = None
 
-    @property
-    def status(self) -> None | str:
-        """ The status of each parent discovered. """
+		# Attributes for Step 2 (Checking Trio/Mating)
+		self.__num_common_mat = None
+		self.__num_conflicts_mat = None
+		self.__perc_conflicts_mat = None
+		self.__status_mat = None
 
-        if self.__perc_conflicts is not None:
-            if 0 <= self.__perc_conflicts < 1:
-                return 'Discovered'
-            elif 1 < self.__perc_conflicts < 3:
-                return 'Doubtful'
-            elif self.__perc_conflicts >= 3:
-                return 'Excluded'
-            else:
-                return None
+	@property
+	def status_sing(self) -> pd.Series | str | None:
+		""" The status of each parent discovered (Step 1). """
+		return self.__status_sing
 
-    @property
-    def num_conflicts(self) -> None | int:
-        return self.__num_conflicts
+	@property
+	def status_mating(self) -> str | None:
+		""" The status of the mating combination (Step 2). """
+		return self.__status_mat
 
-    @property
-    def perc_conflicts(self) -> None | float:
-        return self.__perc_conflicts
+	@property
+	def num_conflicts_sing(self) -> pd.Series | int | None:
+		return self.__num_conflicts_sing
 
-    def search_parent(
-            self,
-            data: pd.DataFrame,
-            descendant: str,
-            parents: str,
-            snp_name_col: str
-    ) -> None:
-        """ Search for paternity.
+	@property
+	def perc_conflicts_sing(self) -> pd.Series | float | None:
+		return self.__perc_conflicts_sing
 
-        :param data: SNP data for descendant and parent.
-        :param descendant: Columns name of the descendant in the data.
-        :param parents: Columns name or list name of the parents in the data.
-        :param snp_name_col: SNP columns name is data.
-        """
+	def search_parent(
+			self,
+			data: pd.DataFrame,
+			descendant: str,
+			parents: str,
+			snp_name_col: str
+	) -> None:
+		""" Search for paternity (Step 1).
 
-        if self.__isag_markers is None:
-            raise ValueError("Error. No array of snp names to verify")
+		:param data: SNP data for descendant and parent(s).
+		:param descendant: Column name of the descendant in the data.
+		:param parents: Column name of column names of the parents in
+		the data.
+		:param snp_name_col: SNP column name in data.
+		"""
+		if self._isag_markers is None:
+			raise ValueError("Error. No array of snp names to verify")
 
-        sample_by_markers = data.loc[
-            data[snp_name_col].isin(self.__isag_markers),
-            [snp_name_col, descendant, parents]
-        ]
+		# 1. Sampling isag markers and clearing data from gaps (5)
+		sample_by_markers = data.loc[
+			data[snp_name_col].isin(self._isag_markers) & \
+			((data[descendant] != 5) & (data[parents] != 5))
+		].set_index(snp_name_col)
 
-        # Filtering 5s from a descendent
-        desc_marks = sample_by_markers.loc[
-            sample_by_markers[descendant] != 5, [snp_name_col, descendant]
-        ]
+		self.__num_common_sing = sample_by_markers.shape[0]
 
-        # According to ICAR, the number of available markers must be
-        # above 450
-        if len(desc_marks) < 450:
-            raise Exception("Calf call rate is low.")
+		# 2. We count conflicts (both are homozygous for different alleles,
+		# for example, 0 and 2)
+		self.__num_conflicts_sing = (
+			(sample_by_markers[parents] - sample_by_markers[descendant]).abs() == 2
+		).sum()
 
-        # Common after filtering markers of potential ancestors
-        sample_parents = sample_by_markers.loc[
-            sample_by_markers[snp_name_col].isin(desc_marks[snp_name_col]),
-            parents
-        ]
+		# 3. We calculate the percentage of conflicts
+		if self.__num_common_sing != 0:
+			self.__perc_conflicts_sing = (
+				(self.__num_conflicts_sing / self.__num_common_sing) * 100
+			).round(2)
 
-        # Number of available markers in potential ancestors
-        prob_parents_same_n_markers = (sample_parents < 5).sum()
+		# 4. We assign statuses according to the rules of ICAR
+		if self.__num_common_sing < 400:
+			self.__status_sing = 'Not Discovered'
+		else:
+			if self.__perc_conflicts_sing is not None:
+				if 0 <= self.__perc_conflicts_sing <= 0.5:
+					self.__status_sing = 'Discovered'
+				elif 0.5 < self.__perc_conflicts_sing <= 2.0:
+					self.__status_sing = 'Possible'
+				elif self.__perc_conflicts_sing > 2:
+					self.__status_sing = 'Excluded'
+				else:
+					self.__status_sing = None
 
-        # number of conflicts
-        self.__num_conflicts = (
-            abs(sample_parents.sub(desc_marks[descendant], axis=0)) == 2
-        ).sum()
+	def search_mating(
+			self,
+			data: pd.DataFrame,
+			descendant: str,
+			sire: str,
+			dam: str,
+			snp_name_col: str
+	) -> None:
+		""" Verify mating combination / Trio (Step 2).
 
-        # Percentage of conflicts
-        self.__perc_conflicts = (
-            (self.__num_conflicts / prob_parents_same_n_markers) * 100
-        ).round(2)
+		:param data: SNP data for descendant, sire, and dam.
+		:param descendant: Column name of the descendant.
+		:param sire: Column name of the sire.
+		:param dam: Column name of the dam.
+		:param snp_name_col: SNP column name in data.
+		"""
+		if self._isag_markers is None:
+			raise ValueError("Error. No array of snp names to verify")
 
-    def __status_define(self) -> None:
-        ...
+		sample_by_markers = data.loc[
+			data[snp_name_col].isin(self._isag_markers)
+		].set_index(snp_name_col)
+
+		desc_vals = sample_by_markers[descendant]
+		sire_vals = sample_by_markers[sire]
+		dam_vals = sample_by_markers[dam]
+
+		# Общие валидные SNP для трио
+		valid_desc = desc_vals != 5
+		valid_sire = sire_vals != 5
+		valid_dam = dam_vals != 5
+		common_valid = valid_desc & valid_sire & valid_dam
+
+		self.__num_common_mat = common_valid.sum()
+
+		if self.__num_common_mat < 400:
+			self.__num_conflicts_mat = 0
+			self.__perc_conflicts_mat = None
+			self.__status_mat = 'Not Checked'
+			return
+
+		# Конфликты для трио: оба родителя гомозиготы по ОДНОМУ аллелю,
+		# а потомок гетерозиготен
+		parents_homozygous_same = (sire_vals.isin([0, 2])) & (
+			dam_vals.isin([0, 2])) & (sire_vals == dam_vals)
+		progeny_heterozygous = (desc_vals == 1)
+
+		conflicts = parents_homozygous_same & progeny_heterozygous & common_valid
+		self.__num_conflicts_mat = conflicts.sum()
+
+		perc_conflicts = (self.__num_conflicts_mat / self.__num_common_mat) * 100
+		self.__perc_conflicts_mat = round(perc_conflicts, 2)
+
+		# Статусы для Шага 2 согласно ICAR Feb 2025
+		if 0 < perc_conflicts <= 1.0:
+			self.__status_mat = 'Confirmed'
+		elif 1 < perc_conflicts <= 4.0:
+			self.__status_mat = 'Possible'
+		elif perc_conflicts > 4.0:
+			self.__status_mat = 'Excluded'
+		else:
+			self.__status_mat = None
--- src/snplib/parentage/_verif.py
+++ src/snplib/parentage/_verif.py
@@ -7,7 +7,8 @@ import pandas as pd
 
 
 """
-https://www.icar.org/Documents/GenoEx/ICAR%20Guidelines%20for%20Parentage%20Verification%20and%20Parentage%20Discovery%20based%20on%20SNP.pdf    
+https://www.icar.org/Documents/GenoEx/ICAR%20Guidelines%20for%20Parentage%20Verification%20and%20Parentage%20Discovery%20based%20on%20SNP.pdf
+https://www.icar.org/wp-content/uploads/documents/Guidelines-for-Parentage-Verification-and-Parentage-Discovery-Based-on-SNP-Genotypes-Feb.-2025.pdf    
 """
 
 
@@ -19,31 +20,31 @@ class Verification(object):
     """
 
     def __init__(
-            self, isag_marks: pd.Series | list | set | None = None
+            self,
+            isag_marks: pd.Series | list | set | None = None
     ) -> None:
         self.__isag_marks = isag_marks
 
+        # ----- Attributes for Step 1 (Checking one parent) ----
         # The minimum number of SNP available in the profile
         # of each animal and potential parent must be scaled (i.e.: 95%
         # truncated down)
         self.__min_num_snp = 0.95
-        self.__num_conflicts = None  # Number of conflicts
+        self.__num_conflicts_sing = None  # Number of conflicts
+        self._status_sing = None
+
+        # ----- Attributes for Step 2 (Checking Trio/Mating) ----
+        self._status_mat = None
+        self._num_conflicts_mat = None
+        self._num_common_mat = None
 
     @property
-    def status(self) -> None | str:
-        if self.__num_conflicts is not None:
-            if self.__num_conflicts <= 2:
-                return 'Accept'
-            elif 3 <= self.__num_conflicts <= 5:
-                return 'Doubtful'
-            elif self.__num_conflicts > 5:
-                return 'Excluded'
-            else:
-                return None
+    def status_sing(self) -> None | str:
+        return self._status_sing
 
     @property
-    def num_conflicts(self) -> None | int:
-        return self.__num_conflicts
+    def num_conflicts_sing(self) -> None | int:
+        return self.__num_conflicts_sing
 
     def check_on(
             self,
@@ -64,6 +65,8 @@ class Verification(object):
             raise ValueError('Error. No array of snp names to verify')
 
         num_isag_mark = len(self.__isag_marks)
+        min_available = np.floor(num_isag_mark * self.__min_num_snp)
+
         min_num_comm_snp = int(num_isag_mark - (2 * (num_isag_mark * 0.05)))
 
         sample_mark = data.loc[
@@ -74,18 +77,91 @@ class Verification(object):
         desc_n_markers = (sample_mark[descendant] < 5).sum()
         parent_n_markers = (sample_mark[parent] < 5).sum()
 
+        # 1. Checking SNP availability (Call Rate)
         # According to ICAR, the number of markers not 5ok should be more
         # than 95%
-        if (desc_n_markers < num_isag_mark * self.__min_num_snp) and \
-                (parent_n_markers < num_isag_mark * self.__min_num_snp):
-            raise Exception('Calf and parent have low call rate')
+        if (desc_n_markers < min_available) and \
+                (parent_n_markers < min_available):
+            self._status_sing = 'Not Verified'
+            self.__num_conflicts_sing = None
+            return
 
+        # 2. Search for common valid SNPs
         comm_snp_no_missing = sample_mark.replace(5, np.nan).dropna()
         num_comm_markers = len(comm_snp_no_missing)
 
         if num_comm_markers < min_num_comm_snp:
-            raise Exception('Pair call rate is low')
+            self._status_sing = 'Not Checked'
+            self.__num_conflicts_sing = None
+            return
 
-        self.__num_conflicts = (abs(
+        # 3. Counting conflicts (both homozygotes for different alleles:
+        # 0 and 2)
+        self.__num_conflicts_sing = (abs(
             comm_snp_no_missing[descendant] - comm_snp_no_missing[parent]
         ) == 2).sum()
+
+        # 4. Determination of status under new ICAR rules
+        if self.__num_conflicts_sing is not None:
+            if self.__num_conflicts_sing <= 2:
+                self._status_sing = 'Accept'
+            elif 3 <= self.__num_conflicts_sing <= 5:
+                self._status_sing = 'Doubtful'
+            elif self.__num_conflicts_sing > 5:
+                self._status_sing = 'Excluded'
+            else:
+                self._status_sing = None
+
+    def check_mating(
+            self,
+            data: pd.DataFrame,
+            descendant: str,
+            sire: str,
+            dam: str,
+            snp_name_col: str
+    ) -> None:
+        """ Verify mating combination / Trio (Step 2). """
+        if self.__isag_marks is None:
+            raise ValueError('Error. No array of snp names to verify')
+
+        total_snps = len(self.__isag_marks)
+        min_available = np.floor(total_snps * 0.95)
+        # Minimum common SNP for trio: Total - 3 * (Total - Min_Available)
+        min_common_trio = total_snps - 3 * (total_snps - min_available)
+
+        sample = data[data[snp_name_col].isin(self.__isag_marks)].set_index(
+            snp_name_col)
+        desc_vals = sample[descendant]
+        sire_vals = sample[sire]
+        dam_vals = sample[dam]
+
+        valid_desc = desc_vals != 5
+        valid_sire = sire_vals != 5
+        valid_dam = dam_vals != 5
+
+        # 1. Finding common valid SNPs for the trio
+        common_valid = valid_desc & valid_sire & valid_dam
+        self._num_common_mat = common_valid.sum()
+
+        if self._num_common_mat < min_common_trio:
+            self._status_mat = 'Not Checked'
+            self._num_conflicts_mat = None
+            return
+
+        # 2. Conflict counting (Parents are homozygous for ONE allele,
+        # offspring is heterozygous)
+        parents_homo_same = (sire_vals.isin([0, 2])) & \
+                            (dam_vals.isin([0, 2])) & \
+                            (sire_vals == dam_vals)
+        progeny_hetero = (desc_vals == 1)
+
+        conflicts = parents_homo_same & progeny_hetero & common_valid
+        self._num_conflicts_mat = conflicts.sum()
+
+        # 3. Определение статуса согласно ICAR Feb 2025
+        if self._num_conflicts_mat <= 3:
+            self._status_mat = 'Mating Accepted'
+        elif self._num_conflicts_mat <= 7:
+            self._status_mat = 'Mating Doubtful'
+        else:
+            self._status_mat = 'Mating Excluded'
